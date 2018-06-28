841988ae2e78f153fb1299d2efaabb553eca2404
Hasol Im <jerubball@gmail.com>
2018-06-28 00:14:03 -0400

    update

--- ExtendedConsoleCommands/modinfo.lua
+++ ExtendedConsoleCommands/modinfo.lua
@@ -1,17 +1,12 @@
 name = "Extended Console Commands"
 description = 
-[[[h1][b]In-game description[/b][/h1]
-Provides multiplayer console commands for DST.
+[[Provides multiplayer console commands for DST.
 This enhances some of built-in consle commands that strts with c_
 New commands start with x_
 
-[h1][b]Overview[/b][/h1]
-This mod introduces new functions which can be used by server admins.
-Enhancements are made based on existing "consolecommands.lua" script.
-
 Source code and detailed description can be found at https://github.com/jerubball/Dont-Starve-Together-Server-Tools/tree/master/ExtendedConsoleCommands]]
 author = "jerubball"
-version = "1.1.13"
+version = "1.1.14"
 
 icon_atlas = "modicon.xml"
 icon = "modicon.tex"
