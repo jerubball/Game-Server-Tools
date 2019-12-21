37ef3ca6f0e2cd1e6a64ffa1f511764e868baf5a
Hasol Im <jerubball@gmail.com>
2019-12-20 23:58:50 -0500

    Update start.sh

--- Ngrok/scripts/start.sh
+++ Ngrok/scripts/start.sh
@@ -1,4 +1,56 @@
 #!/bin/bash
 
-./easyscreen.sh start ngrok ./ngrok start ssh ssh14 ssh23 minecraft19
+args=$(getopt -q -s bash -l "help,restart,silent,nofallback" "?hrsn" "$@")
+eval set -- "$args"
 
+restart=1
+nofallback=1
+mode=start
+
+while [[ $# > 0 ]]
+do
+    # option processing
+    case $1 in
+        -h | --help | -? )
+            echo "[--restart] [--silent] [--nofallback]"
+            exit
+        ;;
+        -r | --restart )
+            restart=0
+        ;;
+        -s | --silent )
+            mode=silent
+        ;;
+        -n | --nofallback )
+            fallback=0
+        ;;
+        -- )
+            shift
+            break
+    esac
+    shift
+done
+
+cd /home/ieee/.ngrok2
+
+if [[ restart -eq 0 ]]
+then
+    ./easyscreen.sh stop ngrok
+fi
+
+tunnels=$(cat tunnels)
+
+if [[ "$tunnels" != "" ]]
+then
+    ./easyscreen.sh $mode ngrok ./ngrok start $tunnels
+fi
+
+if [[ nofallback -eq 0 && "$(screen -ls "$name" | grep "$name")" == "" ]]
+then
+    ./easyscreen.sh $mode ngrok ./ngrok start ssh
+    
+    if [[ "$(screen -ls "$name" | grep "$name")" == "" ]]
+    then
+        ./easyscreen.sh $mode ngrok ./ngrok tcp 22
+    fi
+fi
