c8d1df0a133b5fd0fb700e949151599a542cfc23
Hasol Im <jerubball@gmail.com>
2021-01-20 00:09:56 -0500

    Update start.bat

--- Minecraft/scripts-spigot/start.bat
+++ Minecraft/scripts-spigot/start.bat
@@ -1,3 +1,3 @@
-start "MinecraftServer" /HIGH /WAIT /B java -Xmx4300M -Xms4300M -Xmn4300M -jar spigot-1.16.4.jar
+start "MinecraftServer" /HIGH /AFFINITY 0xf /WAIT /B java -Xmx4300M -Xms4300M -Xmn4300M -jar spigot-1.16.4.jar
 
 pause
